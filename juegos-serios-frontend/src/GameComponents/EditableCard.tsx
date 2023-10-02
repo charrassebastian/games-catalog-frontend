371ad174e43a865b60a513375bc7aac38f18fa19
Sebastian Charras <sebastianhoraciocharras@gmail.com>
2023-10-02 21:41:52 +0000

    Improved the EditableCard

--- juegos-serios-frontend/src/GameComponents/EditableCard.tsx
+++ juegos-serios-frontend/src/GameComponents/EditableCard.tsx
@@ -1,32 +1,46 @@
 import Game from '../types/Game'
 
 export function EditableCard({ game }: { game: Game }) {
+    // TODO:
+    // allow the user to add areas
+    // allow the user to remove areas
+    // allow the user to edit the missing fields
+    // improve the design
+    // interact with the server
+
+    console.log(game.imageLink)
+
     return (
         <div className="card m-3">
-            <img src={game.imageLink} className="card-img-top text-dark" alt={game.name} />
+            { game.imageLink?.length ? <img src={game.imageLink} className="card-img-top text-dark" alt={game.name} /> : null }
             <div className="card-body">
                 <h5 className="card-title text-dark">{game.name}</h5>
-                <label htmlFor='description' className="card-text text-dark">{'Descripción: ' + game.description}</label>
-                <input id="description" />
-                <p className="card-text text-dark">Areas:</p>
+                <label htmlFor={'name-' + game.name} className="form-label">Nombre:</label>
+                <input id={"name" + game.name} type="text" className="form-control" />
+                <label htmlFor={'description' + game.name} className="form-label">Descripción:</label>
+                <input id={"description" + game.name} type="text" className="form-control" />
                 {game.area.map((area: string) => (
                     <>
-                        <label htmlFor={'area-' + area} className="card-text text-dark">{'Area: ' + area}</label>
-                        <input id={'area-' + area} />
+                        <label htmlFor={'area-' + area} className="form-label">{'Area: ' + area}</label>
+                        <input id={'area-' + area} type="text" className="form-control" />
                     </>
                 ))
                 }
-                <label htmlFor="objective" className="card-text text-dark">Tiene un objetivo?:</label>
-                <input id="objective" />
-                <label htmlFor="contentValidation" className="card-text text-dark">Validación de contenido:</label>
-                <input id="contentValidation" />
-                <label htmlFor="observationsAndSuggestions" className="card-text text-dark">Observaciones y sugerencias:</label>
-                <input id="observationsAndSuggestions" />
-                <label htmlFor="score" className="card-text text-dark">Puntaje:</label>
-                <input id="score" />
-                <label htmlFor="other" className="card-text text-dark">Otro:</label>
-                <input id="other" />
-                <a href={game.link} className="btn btn-primary">Abrir pagina web</a>
+                <label htmlFor='newArea' className="form-label">Nueva area</label>  
+                <input id='newArea' type="text" className="form-control" />
+                <label htmlFor={"objective" + game.name} className="form-check-label">Tiene un objetivo?:</label>
+                <input id={"objective" + game.name} type="checkbox" className="form-check-input" />
+                <label htmlFor={"contentValidation" + game.name} className="form-label">Validación de contenido:</label>
+                <input id={"contentValidation" + game.name} type="text" className="form-control" />
+                <label htmlFor={"observationsAndSuggestions" + game.name} className="form-label">Observaciones y sugerencias:</label>
+                <input id={"observationsAndSuggestions" + game.name} type="text" className="form-control" />
+                <label htmlFor={"score" + game.name} className="form-label">Puntaje:</label>
+                <input id={"score" + game.name} type="text" className="form-control" />
+                <label htmlFor={"other" + game.name} className="form-label">Otro:</label>
+                <input id={"other" + game.name} type="text" className="form-control" />
+                <label htmlFor={"imageUrl" + game.name} className="form-label">URL de la imagen:</label>
+                <input id={"imageUrl" + game.name} type="text" className="form-control" />
+                <a href={game.link} className="btn btn-primary mt-3">Abrir pagina web</a>
             </div>
         </div>
     )
