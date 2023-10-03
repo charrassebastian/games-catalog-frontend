920223760a7ce7e0c5a278d07a6b0e24c263c088
Sebastian Horacio Charras <sebastiancharras@outlook.com>
2023-10-03 15:20:09 +0000

    Started adding the authentication

--- juegos-serios-frontend/src/App.tsx
+++ juegos-serios-frontend/src/App.tsx
@@ -1,24 +1,21 @@
-import './App.css'
-import { WelcomeScreen } from './WelcomeScreen/WelcomeScreen'
+import React from 'react';
+import { AuthProvider } from './contexts/AuthContext'; // Asegúrate de ajustar la ruta correcta al AuthContext
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './App.css';
+import { WelcomeScreen } from './WelcomeScreen/WelcomeScreen';
 
-/**
- * TANSTACK CONNECTION TO THE BACKEND FROM THE FRONTEND
- */
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
-
   return (
+    // Integra el proveedor del contexto de autenticación y el proveedor de React Query alrededor de tus componentes
+    <AuthProvider>
       <QueryClientProvider client={queryClient}>
         <WelcomeScreen />
+        {/* Otros componentes */}
       </QueryClientProvider>
-
-  )
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
--- juegos-serios-frontend/src/WelcomeScreen/WelcomeScreen.tsx
+++ juegos-serios-frontend/src/WelcomeScreen/WelcomeScreen.tsx
@@ -1,3 +1,4 @@
+/*
 import { GameList } from '../GameComponents/GameList'
 import axios from 'axios'
 import { baseUrl } from '../constants/url'
@@ -6,8 +7,23 @@ import { Navbar } from '../Navbar/Navbar'
 import { Features } from '../Features/Features'
 import { Footer } from '../Footer/Footer'
 import { MainSection } from '../MainSection/MainSection'
+*/
+
+import React, { useContext, useState } from 'react';
+import { useAuth } from '../contexts/AuthContext';
+import { Navbar } from '../Navbar/Navbar';
+import { Features } from '../Features/Features';
+import { Footer } from '../Footer/Footer';
+import { MainSection } from '../MainSection/MainSection';
+import { EditableCard } from '../GameComponents/EditableCard';
+import { InformationCard } from '../GameComponents/InformationCard';
+import { baseUrl } from '../constants/url'
+
+import axios from 'axios';
+import { GameList } from '../GameComponents/GameList';
 
 export const WelcomeScreen = () => {
+    const { user } = useAuth();
     const getGames = (value: any) => axios.get(baseUrl + 'games', { params: { value }}).then(res => res.data);
     const [games, setGames] = useState([])
     const [searchValue, setSearchValue] = useState("")
@@ -26,17 +42,16 @@ export const WelcomeScreen = () => {
         setGames([])
     }
 
-    // TODO: set based on the availability of the jwt
-    const isEditable = true
+    // Picks what appropiate game component depending on the auth state
+    // const GameCard = user ? EditableCard : InformationCard;
 
-    // If data.games contains games, display them using GameList
     return (
         <>
             <Navbar />
             <MainSection searchValue={searchValue} onSearchValueChange={onSearchValueChange} onSearch={onSearch} onClear={onClear} />
-            <GameList games={games} isEditable={isEditable} />
+            <GameList games={games} isEditable={user ? true : false}/> {/* Usa el componente de juego correspondiente */}
             <Features />
             <Footer />
         </>
-    )
+    );
 }
